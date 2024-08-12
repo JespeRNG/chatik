27c9d1852fa4902d459d9e03a6c2c8981e12aa3f
JespeRNG <viacheslavovich25122001@gmail.com>
2024-08-12 21:40:29 +0300

    add accessToken err handling

--- src/auth/guards/accessToken.guard.ts
+++ src/auth/guards/accessToken.guard.ts
@@ -1,5 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class AccessTokenGuard extends AuthGuard('jwt') {}
+export class AccessTokenGuard extends AuthGuard('jwt') {
+  handleRequest(err, user, info) {
+    if (info?.name === 'TokenExpiredError') {
+      throw new UnauthorizedException('Token expired');
+    } else if (err || !user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
